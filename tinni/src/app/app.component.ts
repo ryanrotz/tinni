a5fe5e82f1ffaf814cb819fdc2969992d5cc7483
Ryan Rotz <rrotz@tirebuyer.com>
2019-02-03 19:45:10 -0800

    added click event to show/hide the Create Itinerary component

--- tinni/src/app/app.component.ts
+++ tinni/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  showCreateItinerary: boolean = false;
+
+  showOrHideCreateItinerary() {
+    this.showCreateItinerary = !this.showCreateItinerary
+  }
 }
